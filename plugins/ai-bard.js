c99ba3f83057bc0a879b0a22f79a4496ed713c33
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-29 22:21:46 -0300

    Update ai-bard.js

--- plugins/ai-bard.js
+++ plugins/ai-bard.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 
 var handler = async (m, { text,  usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, `🌸 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a IanBot-MD?`, m, rcanal)
+if (!text) return conn.reply(m.chat, `🌟 *Ingresé una petición*\n\nEjemplo, ${usedPrefix + command} Conoces a SiskedBot-MD?`, m, rcanal)
 try {
 await m.react('🕒')
 conn.sendPresenceUpdate('composing', m.chat)
