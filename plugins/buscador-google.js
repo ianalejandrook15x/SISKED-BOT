383189941a3a1b222c27fd3bd40da4050e5d5a11
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-29 22:27:30 -0300

    Update buscador-google.js

--- plugins/buscador-google.js
+++ plugins/buscador-google.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
 const text = args.join` `
-if (!text) return conn.reply(m.chat, '🌸 Ingresa lo que deseas buscar en Google.', m, rcanal)
-conn.reply(m.chat, `🍭 Buscando Su Información...`, m, {
+if (!text) return conn.reply(m.chat, '🌟 Ingresa lo que deseas buscar en Google.', m, rcanal)
+conn.reply(m.chat, `🌟 Buscando Su Información...`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
@@ -13,9 +13,9 @@ previewType: 0, thumbnail: icons,
 sourceUrl: channel }}})
 const url = 'https://google.com/search?q=' + encodeURIComponent(text)
 google({'query': text}).then(res => {
-let teks = `🌸 *Resultado de* : ${text}\n\n`
+let teks = `🌟 *Resultado de* : ${text}\n\n`
 for (let g of res) {
-teks += `🍄 *Titulo ∙* ${g.title}\n🚩 *Info ∙* ${g.snippet}\n🔗 *Url ∙* ${g.link}\n\n`
+teks += `🌟 *Titulo ∙* ${g.title}\n🌟 *Info ∙* ${g.snippet}\n🌟 *Url ∙* ${g.link}\n\n`
 }
 conn.reply(m.chat, teks, m, rcanal)
 })
