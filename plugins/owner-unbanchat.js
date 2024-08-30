f1fd1bb79f6fa57a4129f572fb3f406064513fd3
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-30 14:49:07 -0300

    Update owner-unbanchat.js

--- plugins/owner-unbanchat.js
+++ plugins/owner-unbanchat.js
@@ -1,9 +1,9 @@
 let handler = async (m, { conn }) => {
-if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🍭 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
+if (!(m.chat in global.db.data.chats)) return conn.reply(m.chat, '🌟 *¡ESTE CHAT NO ESTÁ REGISTRADO!*', m, fake)
 let chat = global.db.data.chats[m.chat]
-if (!chat.isBanned) return conn.reply(m.chat, '🌸 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
+if (!chat.isBanned) return conn.reply(m.chat, '🌟 *¡LA BOT NO ESTÁ BANEADA EN ESTE CHAT!*', m, fake)
 chat.isBanned = false
-await conn.reply(m.chat, '🌸 *¡EL BOT YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
+await conn.reply(m.chat, '🌟 *¡EL BOT YA FUÉ DESBANEADO EN ESTE CHAT!*', m, fake)
 }
 handler.help = ['unbanchat'];
 handler.tags = ['mods'];
