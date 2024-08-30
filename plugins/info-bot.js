032423d525109f5e4a652287c8e4a03bbed35af1
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-29 23:01:41 -0300

    Update info-bot.js

--- plugins/info-bot.js
+++ plugins/info-bot.js
@@ -5,7 +5,7 @@ handler.all = async function(m) {
 const chat = global.db.data.chats[m.chat];
 if (chat.isBaneed) return
 if (/^bot$/i.test(m.text)) {
-conn.reply(m.chat, `🍂 ¡Hola! Soy Ian, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
+conn.reply(m.chat, `🌟 ¡Hola! Soy Sisked, en que puedo ayudarte hoy?\n\n✰ Usa *.menu* para ver mis comandos.`, m, rcanal, )
 }
 /*if (/^que|q$/i.test(m.text)) {
 conn.reply(m.chat, `*so*`, m, rcanal, )
@@ -18,11 +18,11 @@ if (/^Bot de mierda/i.test(m.text)) {
 conn.reply(m.chat, `*He hey hey skibidi, no digas toilet. Eres perfecta*`, m, rcanal, )
 }
   
-if (/^Corin$/i.test(m.text)) {
-conn.reply(m.chat, `*_Hola buscas algun servidor con precios bajos ? Te presento a CorinPLus un servicio hosting confiable y de buena calidad, entre por aqui https://dash.corinplus.com Gracias por utilizar IanBot-MD_*`, m, rcanal, )
+if (/^que$/i.test(m.text)) {
+conn.reply(m.chat, `*so*`, m, rcanal, )
 }
 if (/^duda$/i.test(m.text)) {
-conn.reply(m.chat, `*Hola tienes alguna duda sobre el bot o sobre el hosting enviame mensaje al privado* ☁ Wa.me/543876639332`, m, rcanal, )
+conn.reply(m.chat, `*Hola tienes alguna duda sobre el bot o sobre el hosting enviame mensaje al privado* ☁ Wa.me/584123989549`, m, rcanal, )
 }
 return !0;
 };
