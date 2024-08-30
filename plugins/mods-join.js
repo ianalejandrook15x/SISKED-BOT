2832bc9ab8fc71a2bfe0596987485a222bc52993
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-30 14:44:58 -0300

    Update mods-join.js

--- plugins/mods-join.js
+++ plugins/mods-join.js
@@ -3,9 +3,9 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 
 let handler = async (m, { conn, text, isOwner, usedPrefix, command }) => {
 
-if (!text) return m.reply(`🌸 Ingresa el enlace del Grupo dice Miguel.`)
+if (!text) return m.reply(`⭐ Ingresa el enlace del Grupo.`)
 let [_, code] = text.match(linkRegex) || []
-if (!code) return m.reply('🌺 Enlace invalido.')
+if (!code) return m.reply('⭐ Enlace invalido.')
 let res = await conn.groupAcceptInvite(code)
 m.reply(`🚩 Me uní correctamente al Grupo`)
 }
