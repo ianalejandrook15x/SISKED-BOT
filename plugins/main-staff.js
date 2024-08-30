d7e0dbaa4fa6ca2d729f72f6b3335dd00b900f4a
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-30 14:42:33 -0300

    Update main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -1,22 +1,22 @@
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-let staff = `🚩 *EQUIPO DE AYUDANTES*
-🌸 *Bot:* ${global.botname}
+let staff = `🍁 *EQUIPO DE AYUDANTES*
+🌟 *Bot:* ${global.botname}
 ✨️  *Versión:* ${global.vs}
 
 👑 *Propietario:*
 
-• Ianalejandrook15x
-🌸 *Rol:* Propietario 👑
-🚩 *Número:* ${creador}
-✨️ *GitHub:* https://github.com/ianalejandrook15x
+• Anuar
+⭐ *Rol:* Propietario 👑
+🌟 *Número:* ${creador}
+✨️ *GitHub:* https://github.com/Sisked-Bot
 
 🌸COLABORADORES
 
-• Miguel
-🌸 *Rol:* Desarrollador ☁
-🚩 *Número:* 543876631319
-✨️ *GitHub:* ---
+• Ian
+⭐ *Rol:* Desarrollador ☁
+🌟 *Número:* 543876631319
+✨️ *GitHub:* https://github.com/ianalejandrook15x
 await conn.sendFile(m.chat, icons, 'nino.jpg', staff.trim(), fkontak, true, {
 contextInfo: {
 'forwardingScore': 200,
