c5cd2709069daecfb2d2fb23ca9db380605cc0e1
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-29 23:04:07 -0300

    Update info-ds.js

--- plugins/info-ds.js
+++ plugins/info-ds.js
@@ -7,7 +7,7 @@ import path from 'path'
 var handler = async (m, { conn, usedPrefix }) => {
 
 if (global.conn.user.jid !== conn.user.jid) {
-return conn.reply(m.chat, '🍂 *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
+return conn.reply(m.chat, '⭐ *Utiliza este comando directamente en el número principal del Bot*', m, rcanal, )
 }
 
 let chatId = m.isGroup ? [m.chat, m.sender] : [m.sender]
@@ -26,14 +26,14 @@ break
 }}}
 
 if (filesDeleted === 0) {
-await conn.reply(m.chat, '🍂 *No se encontró ningún archivo que incluya la ID del chat*', m, rcanal, )
+await conn.reply(m.chat, '🌟 *No se encontró ningún archivo que incluya la ID del chat*', m, rcanal, )
 } else {
-await conn.reply(m.chat, `🌸 *Se eliminaron ${filesDeleted} archivos de sesión*`, m, rcanal, )
-conn.reply(m.chat, `🍂 *¡Hola! ¿logras verme?*`, m, rcanal, )
+await conn.reply(m.chat, `🌟 *Se eliminaron ${filesDeleted} archivos de sesión*`, m, rcanal, )
+conn.reply(m.chat, `🌟 *¡Hola! ¿logras verme?*`, m, rcanal, )
 }
 } catch (err) {
 console.error('Error al leer la carpeta o los archivos de sesión:', err)
-await conn.reply(m.chat, '🍂 *Ocurrió un fallo*', m, rcanal, )
+await conn.reply(m.chat, '🌟 *Ocurrió un fallo*', m, rcanal, )
 }
 
 }
