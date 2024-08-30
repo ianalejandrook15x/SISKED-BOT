2653c5afef6f138e4137975ce8c6a03674bd6088
ianalejandrook15x <sm.iam1836@gmail.com>
2024-08-30 14:43:38 -0300

    Update mods-autoadmin.js

--- plugins/mods-autoadmin.js
+++ plugins/mods-autoadmin.js
@@ -1,14 +1,14 @@
 
 const handler = async (m, {conn, isAdmin, groupMetadata }) => {
-  if (isAdmin) return m.reply('🚩 *¡YA ERES ADM JEFE!*');
+  if (isAdmin) return m.reply('⭐ *¡YA ERES ADM JEFE!*');
   try {
     await conn.groupParticipantsUpdate(m.chat, [m.sender], 'promote');
   await m.react(done)
-   m.reply('🚩 *¡YA TE DI ADM MI JEFE!*');
+   m.reply('⭐ *¡YA TE DI ADM MI JEFE!*');
     let nn = conn.getName(m.sender);
-     conn.reply('543876577197@s.whatsapp.net', `🚩 *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
+     conn.reply('544123989549@s.whatsapp.net', `⭐ *${nn}* se dio Auto Admin en:\n> ${groupMetadata.subject}.`, m, rcanal, );
   } catch {
-    m.reply('🚩 Ocurrio un error.');
+    m.reply('⭐ Ocurrio un error.');
   }
 };
 handler.tags = ['mods'];
