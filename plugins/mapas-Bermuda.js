cf3bf124d11a5bf13d884d4353145a7263de6514
ianalejandrook15x <sm.iam1836@gmail.com>
2024-09-07 23:17:08 -0300

    Update mapas-Bermuda.js

--- plugins/mapas-Bermuda.js
+++ plugins/mapas-Bermuda.js
@@ -22,7 +22,7 @@ const fkontak = {
 await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
 global.db.data.users[m.sender].lastcofre = new Date * 1
 }
-handler.command = ['bermuda', 'mapab', 'mapabermuda'] 
+handler.command = ['bermuda'] 
 handler.register = true
 handler.admin = true
 export default handler
