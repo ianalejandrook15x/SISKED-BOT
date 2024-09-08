ea442b7b650e8fc1fca7613abed11563fab8dfda
ianalejandrook15x <sm.iam1836@gmail.com>
2024-09-07 23:17:44 -0300

    Update mapas-NeXTerra.js

--- plugins/mapas-NeXTerra.js
+++ plugins/mapas-NeXTerra.js
@@ -22,7 +22,7 @@ const fkontak = {
 await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
 global.db.data.users[m.sender].lastcofre = new Date * 1
 }
-handler.command = ['nex', 'nexterra', 'terra'] 
+handler.command = ['nexterra'] 
 handler.register = true
 handler.admin = true
 export default handler
